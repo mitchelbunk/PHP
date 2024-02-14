84b3e6ff472aacaa1b89fae363dae780b4a14e02
MitchelJX <71022184+mitchelbunk@users.noreply.github.com>
2024-02-14 09:26:46 +0100

    Add files via upload
    fullstack project

--- www/fullstack/studentOpleidingen.php
+++ www/fullstack/studentOpleidingen.php
@@ -1,15 +1,28 @@
+<?php
+require "database.php";
+
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Student Opleidingen</title>
-        <meta name="Student Opleidingen">
-        <link rel="stylesheet" href="css/studentOpleidingen.css">
-    </head>
-    <body>
+<head>
+    <title>Student Opleidingen</title>
+    <meta name="Student Opleidingen">
+    <link rel="stylesheet" href="css/studentOpleidingen.css">
+</head>
+<body>
 
-    <?php
-    echo "hello student Opleidingen ";
-    ?>
+<form action="functions/addStudentOpleiding.php" method="post">
+    studentnr:      <br><input type="number"    name="studentnr"><br>
+    opleidingscode:       <br><input type="text"      name="opleidingscode"><br>
+    startdatum:    <br><input type="date"      name="startdatum"><br>
+    einddatum:     <br><input type="date"      name="einddatum"><br>
 
-    </body>
+
+
+
+    <input type="submit" value="Submit" name="insertStudentOpleidingData">
+</form>
+
+<h3><a href="index.html">back</a></h3>
+</body>
 </html>
--- www/fullstack/studenten.php
+++ www/fullstack/studenten.php
@@ -1,26 +1,5 @@
 <?php
-echo 123;
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//phpinfo();
-$mysqli = new mysqli("localhost","root","","phples");
- //
-//// Check connection
-//if ($mysqli -> connect_errno) {
-//    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-//    exit();
-//}
-//print_r($mysqli);
-//
-//try {
-//    $conn = new PDO("mysql:host=$servername;dbname=phples", $username, $password);
-//    // set the PDO error mode to exception
-//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    echo "Connected successfully";
-//} catch(PDOException $e) {
-//    echo "Connection failed: " . $e->getMessage();
-//}
+require "database.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,24 +9,33 @@ $mysqli = new mysqli("localhost","root","","phples");
         <link rel="stylesheet" href="css/studenten.css">
     </head>
     <body>
-        <form>
-            studentnr:<br><input type="number" name="studentnr"><br>
-            roepnaam:<br><input type="text" name="roepnaam"><br>
-            voorletters:<br><input type="text" name="voorletters"><br>
-            tussenvoegsels:<br><input type="text" name="tussenvoegsels"><br>
-            achternaam:<br><input type="text" name="achternaam"><br>
-            adress:<br><input type="text" name="adress"><br>
-            postcode:<br><input type="number" name="postcode"><br>
-            woonplaats:<br><input type="text" name="woonplaats"><br>
-            geslacht:<br><input type="text" name="geslacht"><br>
-            telefoon:<br><input type="number" name="telefoon"><br>
-            geboortedatum:<br><input type="date" name="geboortedatum"><br>
-            uitgeschreven:<br><input type="text" name="uitgeschreven"><br>
-            schoolgeld:<br><input type="number" name="schoolgeld"><br>
-            betaald:<br><input type="number" name="betaald"><br>
 
 
-            <button>submit</button>
+
+        <form action="functions/addStudent.php" method="post">
+            studentnr:      <br><input type="number"    name="studentnr"><br>
+            roepnaam:       <br><input type="text"      name="roepnaam"><br>
+            voorletters:    <br><input type="text"      name="voorletters"><br>
+            tussenvoegsels: <br><input type="text"      name="tussenvoegsels"><br>
+            achternaam:     <br><input type="text"      name="achternaam"><br>
+            adress:         <br><input type="text"      name="adress"><br>
+            postcode:       <br><input type="text"    name="postcode"><br>
+            woonplaats:     <br><input type="text"      name="woonplaats"><br>
+
+            geslacht:       <br><select                 name="geslacht">
+                                    <option value="M">Man</option>
+                                    <option value="V">Vrouw</option>
+                                </select>
+                            <br>
+            telefoon:       <br><input type="number"    name="telefoon"><br>
+            geboortedatum:  <br><input type="date"      name="geboortedatum"><br>
+            uitgeschreven:  <br><input type="date"      name="uitgeschreven"><br>
+            schoolgeld:     <br><input type="number"    name="schoolgeld"><br>
+            betaald:        <br><input type="number"    name="betaald"><br>
+
+
+            <input type="submit" value="Submit" name="insertUserData">
         </form>
+        <h3><a href="index.html">back</a></h3>
     </body>
 </html>
